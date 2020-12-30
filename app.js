f270dd975fdc30cd9a2c14e04422ab2099c11c73
imhamza1 <hamza.akram4635@gmail.com>
2020-12-30 11:01:51 +0500

    cors policy

--- app.js
+++ app.js
@@ -16,6 +16,23 @@ var cors = require("cors");
 
 var app = express();
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  // allow preflight
+  if (req.method === "OPTIONS") {
+    res.send(200);
+  } else {
+    next();
+  }
+});
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
